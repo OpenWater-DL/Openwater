6163aeb3df130830e7d84f6c3f91b96546be457d
water <hdl2100598@126.com>
2021-03-01 10:43:10 +0900

    Site updated: 2021-03-01 10:43:10

--- js/sketch.js
+++ js/sketch.js
@@ -2,14 +2,17 @@ var canvas;
 function setup() {
     canvas=createCanvas(windowWidth, 300);
     noCursor();
+    
   }
   
   function draw() {
     background(220,30);
     fill(220);
-
-
-    rect(random(width),random(height),10,10);
-
+    textSize(32);
+    
+    // rect(random(width),random(height),10,10);
+    fill(255,255,0);
+    text('Design', random(width), random(height));
+    console.log(1, 2, 3); // prints [Embedded page] 1
 
   }
